e84c7bc2c45d3d503b5c8010c76b4e65ebb4b810
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-11-23 00:09:47 +0700

    Add SearchBar in MainActivity

--- app/src/main/java/com/example/firstprojectcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstprojectcompose/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.firstprojectcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.firstprojectcompose.components.SearchBar
 import com.example.firstprojectcompose.ui.theme.FirstProjectComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +25,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FirstProjectComposeTheme {
-                JetCoffeeApp()
-
+                FirstProjectComposeApp()
             }
         }
     }
 }
 
 @Composable
-fun JetCoffeeApp() {
+fun FirstProjectComposeApp() {
+    Column {
+        Banner()
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+){
+    Box(modifier = modifier){
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun JetCoffeeAppPreview() {
+fun FirstProjectComposePreview() {
     FirstProjectComposeTheme {
-        JetCoffeeApp()
+        FirstProjectComposeApp()
     }
 }
