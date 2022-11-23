dd96bffad170994d893305b3f32d8c48bcc0eb2e
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-11-24 05:37:41 +0700

    add category in main

--- app/src/main/java/com/example/firstprojectcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstprojectcompose/MainActivity.kt
@@ -4,20 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.firstprojectcompose.components.CategoryItem
 import com.example.firstprojectcompose.components.SearchBar
+import com.example.firstprojectcompose.components.SectionText
+import com.example.firstprojectcompose.model.dummyCategory
 import com.example.firstprojectcompose.ui.theme.FirstProjectComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,9 +32,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun FirstProjectComposeApp() {
-    Column {
+fun FirstProjectComposeApp(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
         Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -50,6 +52,29 @@ fun Banner(
             modifier = Modifier.height(160.dp)
         )
         SearchBar()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+){
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp), // mengatur jarak antar item
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier.padding(16.dp)
+    ){
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview(){
+    FirstProjectComposeTheme {
+        CategoryRow()
     }
 }
 
